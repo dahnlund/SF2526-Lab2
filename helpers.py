310d5c50281502ec92cf02d503a4d6913a6401d9
dahnlund <david.ahnlund@gmail.com>
2023-02-13 12:33:18 +0100

    updated exercise 3

--- helpers.py
+++ helpers.py
@@ -2,5 +2,5 @@ import numpy as np
 
 def eig(M):   #Create a eig-function to get the values and vectors sorted in a descending order
     eig_vals, eigvectors = np.linalg.eig(M)
-    idx = np.flip(eig_vals.argsort())
+    idx = eig_vals.argsort()
     return eig_vals[idx], eigvectors[:,idx]
